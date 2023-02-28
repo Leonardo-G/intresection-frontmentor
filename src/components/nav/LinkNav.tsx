a3dbcadd1645ddef81a7ba9918bc5b88cb89b8cf
leonardog9823 <guanuco43@gmail.com>
2023-02-27 22:24:25 -0300

    modal link hover

--- src/components/nav/LinkNav.tsx
+++ src/components/nav/LinkNav.tsx
@@ -3,6 +3,9 @@ import React, { FC, useState } from 'react'
 import { IconDefinition } from '@fortawesome/free-regular-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
+import style from "./LinkNav.module.css";
+import { faBell, faCalendar, faCalendarDays, faClockFour } from '@fortawesome/free-solid-svg-icons';
+
 interface Props {
     title: string;
     icon?: IconDefinition;
@@ -11,34 +14,67 @@ interface Props {
 
 export const LinkNav: FC<Props> = ({ icon, title, border }) => {
 
-    //Clase de css en caso de no estar HOVER. 
-    //COL-DARK en caso de estar en HOVER;
-    const [isHover, setIsHover] = useState("col-grey");
+    //Vamos agregar una clase de css en caso de no estar HOVER. 
+    //en caso de estar en HOVER se le cambiara;
+    const [isHover, setIsHover] = useState(false);
 
     if ( icon ){
         return (
             <div 
-                className='flex col-gap-1 pointer py-3 px-5'
-                onMouseOver={ ( ) => setIsHover( "col-dark" ) } 
-                onMouseOut={ ( ) => setIsHover( "col-grey" ) }   
+                className='flex col-gap-1 pointer py-3 px-5 relative'
+                onMouseOver={ ( ) => setIsHover( true ) } 
+                onMouseOut={ ( ) => setIsHover( false ) }   
             >
-                <p className={ isHover }>{ title }</p>
+                <p className={ isHover ? "col-dark" : "col-grey" }>{ title }</p>
                 <FontAwesomeIcon
                     icon={ icon }
                     size="sm"
-                    className={`transition ${ isHover === "col-dark" ? "rotate" : "" } ${isHover} `}
+                    className={`transition ${ isHover ? "rotate" : "" } ${ isHover ? "col-dark" : "col-grey" } `}
                 />
+                {
+                    isHover &&
+                    <div className={` ${style.modalNav}`}>
+                        <div className='flex col-gap-3'>
+                            <FontAwesomeIcon 
+                                icon={ faCalendarDays }
+                                color="#3f51b5"
+                            />
+                            <p>Todo List</p>
+                        </div>
+                        <div className='flex col-gap-3 mt-3'>
+                            <FontAwesomeIcon 
+                                icon={ faCalendar }
+                                color="#03a9f4"
+                            />
+                            <p>Calendar</p>
+                        </div>
+                        <div className='flex col-gap-3 mt-3'>
+                            <FontAwesomeIcon 
+                                icon={ faBell }
+                                color="#cddc39"
+                            />
+                            <p>Reminders</p>
+                        </div>
+                        <div className='flex col-gap-3 mt-3'>
+                            <FontAwesomeIcon 
+                                icon={ faClockFour }
+                                color="hsl(266deg 75% 44%)"
+                            />
+                            <p>Planning</p>
+                        </div>
+                    </div>
+                } 
             </div>
         ) 
     }
 
     return (
-        <div 
-            className={`flex col-gap-1 py-3 px-5 pointer ${ isHover === "col-dark" && border ? "border-dark" : border ? "border-grey" : "" }`}
-            onMouseOver={ ( ) => setIsHover( "col-dark" ) }
-            onMouseOut={ ( ) => setIsHover( "col-grey" ) }
+        <div
+            className={`flex col-gap-1 py-3 px-5 pointer ${ isHover && border ? "border-dark" : border ? "border-grey" : "" }`}
+            onMouseOver={ ( ) => setIsHover( true ) }
+            onMouseOut={ ( ) => setIsHover( false ) }
         >
-            <p className={ isHover }>{ title }</p>
+            <p className={ isHover ? "col-dark" : "col-grey" }>{ title }</p>
         </div>
     )
 }
