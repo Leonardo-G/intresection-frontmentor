fa2504847354020409e5487cbaec5f28eed4908d
leonardog9823 <guanuco43@gmail.com>
2023-03-01 20:26:37 -0300

    nav responsive

--- src/components/nav/LinkNav.tsx
+++ src/components/nav/LinkNav.tsx
@@ -3,22 +3,72 @@ import React, { FC, useState } from 'react'
 import { IconDefinition } from '@fortawesome/free-regular-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-import style from "./LinkNav.module.css";
-import { faBell, faCalendar, faCalendarDays, faClockFour } from '@fortawesome/free-solid-svg-icons';
 import { links } from 'database/links';
+
+import style from "./LinkNav.module.css";
 
 interface Props {
     title: string;
     icon?: IconDefinition;
     border?: boolean;
+    block?: boolean;
+    center?: boolean;
 }
 
-export const LinkNav: FC<Props> = ({ icon, title, border }) => {
+export const LinkNav: FC<Props> = ({ icon, title, border, block, center }) => {
 
     //Vamos agregar una clase de css en caso de no estar HOVER. 
     //en caso de estar en HOVER se le cambiara;
     const [isHover, setIsHover] = useState(false);
+    const [isBlock, setIsBlock] = useState(false);
     const [modalLinksState, setModalLinksState] = useState(links.filter( l => l.title === title )[0]);
+
+    if ( block && icon ){
+        return (
+            <>
+                <div 
+                    className='flex col-gap-1 pointer py-3 px-5 '
+                    onClick={ ( ) => setIsBlock( !isBlock ) }   
+                >
+                    <p className={ isHover ? "col-dark" : "col-grey" }>{ title }</p>
+                    <FontAwesomeIcon
+                        icon={ icon }
+                        size="sm"
+                        className={`transition ${ isHover ? "rotate" : "" } ${ isHover ? "col-dark" : "col-grey" } `}
+                    />
+                    
+                </div>
+                {
+                    isBlock && modalLinksState.modalLinks?.find( m => m.icon ) 
+                    ?
+                        <div className={` ${style['modalNav-block']}`}>
+                            {
+                                modalLinksState.modalLinks.map( (m, idx) => (
+                                    <div key={ m.title } className={`flex col-gap-3 ${ idx !== 0 ? "mt-3" : "" }`}>
+                                        <FontAwesomeIcon 
+                                            icon={ m.icon! }
+                                            color={ m.color }
+                                        />
+                                        <p>{ m.title }</p>
+                                    </div>
+                                ))
+                            }
+                        </div>
+                    : isBlock && modalLinksState.modalLinks &&
+                        <div className={` ${style['modalNav-block']}`}>
+                            {
+                                modalLinksState.modalLinks.map( (m, idx )=> (
+                                    <div key={ m.title } className={`flex col-gap-3 ${ idx !== 0 ? "mt-3" : "" }`}>
+                                        <p>{ m.title }</p>
+                                    </div>
+                                ))
+                            }
+                        </div>
+                } 
+            </>
+        ) 
+    }
+
 
     if ( icon ){
         return (
@@ -66,11 +116,11 @@ export const LinkNav: FC<Props> = ({ icon, title, border }) => {
 
     return (
         <div
-            className={`flex col-gap-1 py-3 px-5 pointer ${ isHover && border ? "border-dark" : border ? "border-grey" : "" }`}
+            className={`py-3 px-5 pointer ${ isHover && border ? "border-dark" : border ? "border-grey" : "" }`}
             onMouseOver={ ( ) => setIsHover( true ) }
             onMouseOut={ ( ) => setIsHover( false ) }
         >
-            <p className={ isHover ? "col-dark" : "col-grey" }>{ title }</p>
+            <p className={`${ center ? "txt-center" : "" } ${isHover ? "col-dark" : "col-grey"} `}>{ title }</p>
         </div>
     )
 }
--- src/components/nav/NavBar.tsx
+++ src/components/nav/NavBar.tsx
@@ -1,27 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars, faXmark } from '@fortawesome/free-solid-svg-icons';
 
 import { links } from 'database/links';
 import { LinkNav } from './LinkNav';
 
+import style from "./NavBar.module.css";
 
 export const NavBar = () => {
+
+    const [openNav, setOpenNav] = useState(false);
+
     return (
         <header>
             <nav className='flex p-4 f-auto s-between col-center'>
                 <div className='flex col-center col-gap-5'>
                     <p className='size-6 bold'>snap</p>
-                    <div className='flex'>
-                     {
-                        links.map( l => (
-                            <LinkNav 
-                                key={ l.title }
-                                { ...l }
-                            />
-                        ))
-                    }
+                    <div className='flex d-none-xl'>
+                        {
+                            links.map( l => (
+                                <LinkNav 
+                                    key={ l.title }
+                                    { ...l }
+                                />
+                            ))
+                        }
                     </div>
                 </div>
-                <div className='flex'>
+                <div className='flex d-none-xl'>
                     <LinkNav 
                         title='Login'
                     />
@@ -30,7 +37,49 @@ export const NavBar = () => {
                         border
                     />
                 </div>
+                <div 
+                    className='d-none d-visible-xl pointer'
+                    onClick={ () => setOpenNav( true ) }
+                >
+                    <FontAwesomeIcon 
+                        icon={ faBars }
+                        size="2x"
+                    />
+                </div>
             </nav>
+            <div className={` ${ style['nav-responsive'] } ${ openNav ? style['nav-responsive-open'] : ""} `}>
+                <div 
+                    className='txt-right pointer'
+                    onClick={ () => setOpenNav( false ) }
+                >
+                    <FontAwesomeIcon 
+                        icon={ faXmark }
+                        size="2x"
+                    />
+                </div> 
+                <div className='flex f-column mt-5'>
+                    {
+                        links.map( l => (
+                            <LinkNav 
+                                key={ l.title }
+                                { ...l }
+                                block
+                            />
+                        ))
+                    }
+                    <div className='mt-5'>
+                        <LinkNav 
+                            title='Login'
+                            center
+                        />
+                        <LinkNav 
+                            title='Register'
+                            border
+                            center
+                        />
+                    </div>
+                </div>   
+            </div>
         </header>
     )
 }
