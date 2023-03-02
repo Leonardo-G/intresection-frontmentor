83edab3e974ad15299d17e9f8a09d564714d5ac6
leonardog9823 <guanuco43@gmail.com>
2023-03-02 00:32:37 -0300

    responsive front

--- src/components/sections/Front.tsx
+++ src/components/sections/Front.tsx
@@ -1,25 +1,29 @@
+import React from 'react';
 import Image from 'next/image'
-import React from 'react'
+
+import style from "./Front.module.css"
 
 export const Front = () => {
     return (
-        <div className='flex col-center mt-3'>
-            <div className='f-auto'>
-                <h1 className='h1'>Make remote work</h1>
-                <p className='mt-5 size-2 col-grey width80'>
+        <div className='flex col-center mt-3 f-column-rev-xl'>
+            <div className='f-auto mt-5'>
+                <h1 className='h1 txt-center-xl'>Make remote work</h1>
+                <p className='mt-5 size-2 col-grey width80 txt-center-xl size-auto-xs'>
                     Get your team in sync, no matter your location.
                     Streamline precesses, create team rituals, and 
                     watch productivity soar.
                 </p>
-                <button className='mt-5 btn btn-dark btn-hover-transparent'>Learn more</button>
+                <div className='txt-center-xl'>
+                    <button className='mt-5 btn btn-dark btn-hover-transparent'>Learn more</button>
+                </div>
             </div>
-            <div className='relative f-auto' style={{ width: "100%", height: "700px"}}>
+            <div className={ style['container-image'] }>
                 <Image 
                     src="/image-hero-desktop.png"
                     alt='Imagen Portada'
                     fill
                     className='contain'
-                />  
+                />
             </div>
         </div>
     )
